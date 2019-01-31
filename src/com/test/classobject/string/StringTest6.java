e6a65ccbfcaa527284286a6e7dc630309f7f094f
Niranjan <nirpanig@publicisgroupe.net>
2019-01-31 22:34:17 +0530

    adding files

---
+++ src/com/test/classobject/string/StringTest6.java
@@ -0,0 +1,12 @@
+package com.test.classobject.string;
+
+public class StringTest6 {
+	public static void main(String[] args) {
+		final int i =5;
+		final int j=8;
+		for (int j2 = 0; j < i; j2++) {
+			System.out.println("hello");
+		}
+	}
+
+}
